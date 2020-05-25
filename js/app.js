9563dfe5af81bc3ac36a67c9bff582fee1c18324
Bhagya Patel <60622922+bhagya2002@users.noreply.github.com>
2020-05-25 00:32:31 -0600

    updates the stats using the data collected  form the API

--- js/app.js
+++ js/app.js
@@ -44,7 +44,39 @@ function fetchData(user_country) {
 			dates.forEach(date => {
 				let DATA = data[date];
 				app_data.push(DATA)
+				cases_list.push(parseInt(DATA.total_cases.replace(/,/g, "")));
+				recovered_list.push(parseInt(DATA.total_recovered.replace(/,/g, "")));
+				deaths_list.push(parseInt(DATA.total_deaths.replace(/,/g, "")));
 			})
+		})
+		.then(() => {
+			updateUI();
+		})
+		.catch(error => {
+			alert(error);
 		})
 }
 fetchData(user_country);
+
+// update the user inteface
+function updateUI() {
+	updateStats();
+	// axesLinearChart();
+}
+
+function updateStats() {
+	let last_entry = app_data[app_data.length - 1];
+	let before_last_entry = app_data[app_data.length - 2];
+	console.log(last_entry);
+
+	country_name_element.innerHTML = last_entry.country_name;
+
+	total_cases_element.innerHTML = last_entry.total_cases || 0;
+	new_cases_element.innerHTML = `+${last_entry.new_cases || 0}`
+
+	recovered_element.innerHTML = last_entry.total_recovered;
+	new_recovered_element.innerHTML = `+${parseInt(last_entry.total_recovered.replace(/,/g, "")) - parseInt(before_last_entry.total_recovered.replace(/,/g, ""))}`;
+
+	deaths_element.innerHTML = last_entry.total_deaths;
+	new_deaths_element.innerHTML = `+${parseInt(last_entry.total_deaths.replace(/,/g, "")) - parseInt(before_last_entry.total_deaths.replace(/,/g, ""))}`;
+}
