cf8a02630e9428c56118cd00b2c28dc6ddf7d6a2
Bhagya Patel <60622922+bhagya2002@users.noreply.github.com>
2020-05-24 22:58:23 -0600

    update the # of cases

--- js/app.js
+++ js/app.js
@@ -1,9 +1,50 @@
-/*
-fetch(`https://covid19-monitor-pro.p.rapidapi.com/coronavirus/cases_by_days_by_country.php?country=country`, {
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "covid19-monitor-pro.p.rapidapi.com",
-			"x-rapidapi-key": "7e269ec140msh8a5df9cfc21b4b4p1c1e3ejsn9aba26afc6e0"
-		}
-	})
-*/
+// select the elements
+const country_name_element = document.querySelector(".country .name");
+const total_cases_element = document.querySelector(".total-cases .value");
+const new_cases_element = document.querySelector(".total-cases .new-value");
+const recovered_element = document.querySelector(".recovered .value");
+const new_recovered_element = document.querySelector(".recovered .new-value");
+const deaths_element = document.querySelector(".deaths .value");
+const new_deaths_element = document.querySelector(".deaths .new-value");
+
+
+const ctx = document.querySelector("canvas").getContext("2d");
+
+// DATA collected
+let app_data = [],
+	cases_list = [],
+	recovered_list = [],
+	deaths_list = [],
+	dates = [];
+
+// get the users country code
+let country_code = geoplugin_countryCode();
+let user_country;
+
+country_list.forEach(country => {
+	if (country.code == country_code) {
+		user_country = country.name;
+	}
+});
+
+
+function fetchData(user_country) {
+	fetch(`https://covid19-monitor-pro.p.rapidapi.com/coronavirus/cases_by_days_by_country.php?country=${user_country}`, {
+			"method": "GET",
+			"headers": {
+				"x-rapidapi-host": "covid19-monitor-pro.p.rapidapi.com",
+				"x-rapidapi-key": "11f9888308msh1a247f9e34000b6p1c316fjsned0bcd69a910"
+			}
+		})
+		.then(response => {
+			return response.json();
+		}).then(data => {
+			dates = Object.keys(data);
+
+			dates.forEach(date => {
+				let DATA = data[date];
+				app_data.push(DATA)
+			})
+		})
+}
+fetchData(user_country);
